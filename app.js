47f5171ef5265b34f3adf0bcc4b094f20ec03f8e
alangale <alangabrielale@gmail.com>
2023-07-31 22:52:22 -0300

    Agregue a orientacion a objetos y algunos metodos de operacion sobre array (map,foreach,indexof,join)

--- app.js
+++ app.js
@@ -1,34 +1,57 @@
+class player{
+    constructor (name,posicion){
+        this.name = name
+        this.posicion = posicion
+    }
+}
 
-const agregarPlayer = (nombre,posicion) => {
-    alert("Jugador "+nombre+" que juega de "+posicion+" registrado correctamente")
-    jugadores.push([nombre,posicion])
+class playerController{
+    constructor(){
+        this.listaPlayers = []
+    }
 
-  }
-
-  const eliminarPlayer = (nombre) => {  
-    alert(productos.find(producto => nombre==producto))//usar includes
+    agregarPlayer(player) {
+        alert("Jugador "+player.name+" que juega de "+player.posicion+" registrado correctamente")
+        this.listaPlayers.push(player)
+        
+      }
     
-  }
+    eliminarPlayer(nombre){  
+      
+        this.listaPlayers.forEach(el=>{
+            if(el.name == nombre){
+                let id = this.listaPlayers.indexOf(el)
+                this.listaPlayers.splice(id, 1);
+            }
 
+        })
 
+        return this.listaPlayers
+      }
+    
+
+      mostrarPlayer(){
+       return this.listaPlayers.map(player => player.name)
+      }
+    }
 
 let decision 
 let salir 
 let jugadores = []
+const controlador_players = new playerController()
 
 while(salir!=true){
     decision=3
     while(decision !=1 && decision !=2){
 
-    decision = prompt("Jugadores\n¿Qué desea hacer?\n1-Agregar jugador\n2-Eliminar jugador\n3-Ingrese `delanteros` para verlos\n3-Ingrese `defensores` para verlos\n0-Salir")
+    decision = prompt("Jugadores\n¿Qué desea hacer?\n1-Agregar jugador\n2-Mostrar jugador\n3-Eliminar jugador\n0-Salir")
 
     if(decision==0)
     {
        salir=true;
        listado= jugadores.join("\n")
-       
-       listado = listado.replaceAll(",","             ")
-       alert("Ingresó los siguientes jugadores y su posición: \n JUGADOR       POSICION\n" + listado)
+       listado = controlador_players.mostrarPlayer()
+       alert("Quedaron registrados los siguientes jugadores: \n" + listado)
         alert("Proceso finalizado. Para hacer otra acción vuelva a ingresar al sitio")
         break
         
@@ -38,29 +61,23 @@ while(salir!=true){
         {
             let nombre = prompt("Ingrese nombre de jugador")
             let valor = prompt("Ingrese posicion")
-            agregarPlayer(nombre,valor)
+            
+            playerNew = new player(nombre,valor)
+            controlador_players.agregarPlayer(playerNew)
 
             
         }
 
-            else if(decision==2)
-            {
-                let nombre = prompt("Ingrese nombre de producto a eliminar")
-                eliminarPlayer(nombre)
-            }
-                else if(decision=="delanteros" || decision=="defensores"){
-                   
-                    filtro = jugadores.filter(posicion => posicion = decision)
-                  
-                    filtro.forEach(el => {
-                        console.log(el[0])
-                    });
-                }
-                    else if(decision=="listar"){
-                        
-                        const listado = jugadores.map(jugador => jugador[0])
-                        alert(listado)
-                    }
+        else if(decision==2)
+        {
+           alert(controlador_players.mostrarPlayer())
+        }
+            
+        else if(decision == 3){
+           let nombre = prompt("Ingrese nombre de jugador a eliminar")
+            controlador_players.eliminarPlayer(nombre)
+        }
 
+    
                 }
 }
